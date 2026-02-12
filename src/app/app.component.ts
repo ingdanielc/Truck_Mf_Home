ef56825ff402f528d97b24e0656b58f2caccbfb8
Daniel Andrés Castro Solis (GTK) <dsolis@gattaca.co>
2026-02-12 18:07:55 -0500

    Update: Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, NavigationEnd, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  ActivatedRoute,
+  NavigationEnd,
+  RouterOutlet,
+} from '@angular/router';
 import pkg from 'package.json';
 import { filter, map } from 'rxjs';
+import { GMenuComponent } from './components/g-menu/g-menu.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GMenuComponent],
 })
 export class AppComponent implements OnInit {
   title = 'CashTruck';
@@ -19,7 +25,7 @@ export class AppComponent implements OnInit {
   constructor(
     private readonly router: Router,
     private readonly activatedRoute: ActivatedRoute,
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.router.events
@@ -33,7 +39,7 @@ export class AppComponent implements OnInit {
         filter((route) => route.outlet === 'primary'),
       )
       .subscribe((route) => {
-        this.tituloMenuBar = 'Strem Force';
+        this.tituloMenuBar = 'CashTruck';
         this.isSidebar = false;
         this.isLogoMenuBar = true;
         if (route.snapshot.url[0].path === 'configuration') {
@@ -41,7 +47,7 @@ export class AppComponent implements OnInit {
           this.isSidebar = true;
         }
         if (route.snapshot.url[0].path === 'home') {
-          this.tituloMenuBar = 'Strem Force';
+          this.tituloMenuBar = 'CashTruck';
         }
       });
   }
