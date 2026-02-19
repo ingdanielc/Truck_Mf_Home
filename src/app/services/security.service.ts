ffd7cdc365836800d666040c569a6e625c71dd26
Daniel Andrés Castro Solis (GTK) <dsolis@gattaca.co>
2026-02-18 23:57:15 -0500

    Update: Se obtiene el rol del usuario logueado

---
+++ src/app/services/security.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SecurityService {
+  basePath: string = environment._APIUrl + '/security';
+
+  constructor(private readonly http: HttpClient) {}
+
+  getUserFilter(filter: any) {
+    const headers = { 'content-type': 'application/json' };
+    const body = JSON.stringify(filter);
+    return this.http.post<any>(`${this.basePath}/filter`, body, {
+      headers: headers,
+    });
+  }
+}
